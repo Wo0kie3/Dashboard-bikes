49960a05cbdb0783fd26bbe55803cd1b826e79ce
Vosloo <marek.szydlow@gmail.com>
2021-05-30 17:48:24 +0200

    Added improved version of rose plot for distance attribute

--- src/plots.py
+++ src/plots.py
@@ -6,51 +6,65 @@ import plotly.express as px
 
 # Retrieve realpath of running script and get only dir name
 data_dir = dirname(realpath(__file__)) + '/../data/'
-
 # Number of bins for rose plot
-no_bins = 16
+no_bins = 20
 
 
 def _find_bin(limits, val):
     """
     Helping function for assign_bins implementing binary search
+    :return: [bin_no, left_bound, right_bound]
     """
     left, right = 0, len(limits) - 1
+    lefts, rights, mids = [left], [right], []
     while left <= right:
         mid = left + (right - left) // 2
+        mids.append(mid)
 
         if right - left == 0:
+            # Return bin and interval
             if val < limits[mid]:
-                # print(f'{val} assiging to bin no.{mid}')
-                return mid
+                return [mid, str(limits[mid - 1]), str(limits[mid])]
             else:
-                # print(f'{val} assiging to bin no.{mid + 1}')
-                return mid + 1
+                right_interval = "inf" if (mid + 1) == len(limits) else str(limits[mid + 1])
+                return [mid + 1, str(limits[mid]), right_interval]
 
         if val == limits[mid]:
             # Return left bin
-            # print(f'{val} assiging to bin no.{mid}')
-            return mid
+            return [mid, str(limits[mid - 1]), str(limits[mid])]
 
         if val < limits[mid]:
             # Get left half
             right = mid - 1
+            lefts.append(-1)
+            rights.append(right)
         else:
             # Get right half
             left = mid + 1
+            lefts.append(left)
+            rights.append(-1)
+
+    return [mid, str(limits[mid - 1]), str(limits[mid])]
 
 
-def assign_bins(values: pd.Series, no_bins) -> pd.DataFrame:
+def assign_bins(values: pd.Series, no_bins, column_names) -> pd.DataFrame:
     """
     Assigns values to bins [1; no_bins]
     """
     limits = np.linspace(values.min(), values.max(), no_bins)
-    return values.apply(lambda val: _find_bin(limits, val))
+    return pd.DataFrame(
+        np.array([_find_bin(limits, val) for val in values.values]),
+        columns=column_names
+    )
 
 
-def plot_rose(df: pd.DataFrame) -> None:
+def plot_rose(df: pd.DataFrame, r, theta, hover_data={}, labels={}, title='') -> None:
+    # TODO: Add docstring
     fig = px.bar_polar(
-        df, r='count', theta='bin'
+        df, r='count', theta='left_bound', labels=labels, 
+        title=title, start_angle=0, template='ggplot2',
+        hover_data={}, 
+        direction='counterclockwise'
     )
 
     fig.show()
@@ -59,15 +73,28 @@ def plot_rose(df: pd.DataFrame) -> None:
 if __name__ == '__main__':
     df = pd.read_csv(data_dir + 'reduced.csv')
 
-    bins = assign_bins(df['distance'], no_bins)
-    distances = pd.DataFrame({
-        'distance': df['distance'],
-        'bin': (360 / no_bins) * bins
-    })
-    distances = distances.groupby(by='bin') \
-        .count()                            \
-        .reset_index()                      \
-        .rename(columns={'distance': 'count'})
+    columns_names = ["bin", "left_bound", "right_bound"]
+    bins = assign_bins(df['distance'], no_bins, columns_names)
 
-    plot_rose(distances)
+    bins['bin'] = pd.to_numeric(bins['bin'])
+    bins['interval'] = " " + bins["left_bound"] + " - " + bins["right_bound"]
+
+    distances = bins.groupby(by=['bin', 'left_bound', 'interval']) \
+        .count()                                                   \
+        .reset_index()                                             \
+        .sort_values(by=['bin'])                                   \
+        .rename(columns={'right_bound': 'count'})
+
+    hover_data = {
+        'interval': True, 
+        'left_bound': False, 
+        'bin': False
+    }
+    labels = {
+        'count': 'No. people', 
+        'interval': 'Distance interval '
+    }
+    title = 'Distance plot, something...'
+
+    plot_rose(distances, 'count', 'left_bound', hover_data, labels, title)
 
