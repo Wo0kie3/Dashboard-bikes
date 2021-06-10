1342043f644ff061bb7c9549361a42088688f404
Vosloo <marek.szydlow@gmail.com>
2021-06-10 20:40:21 +0200

    Added mapbox plot

--- src/plots.py
+++ src/plots.py
@@ -3,69 +3,29 @@ from os.path import dirname, realpath
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.missing_ipywidgets import FigureWidget
+from datasets import create_bins
 
 from utils import file_exists
 
 # Retrieve realpath of running script and get only dir name
 data_dir = dirname(realpath(__file__)) + '/../data/'
+
+# Files
 bin_file = data_dir + "bins.csv"
+station_file = data_dir + "stations.csv"
+seasons_file = data_dir + "seasons.csv"
+mapbox_token = data_dir + ".mapbox_token"
 
 # Number of bins for rose plot
 no_bins = 20
 
 
-def _find_bin(limits, val):
+def plot_rose(df: pd.DataFrame, r, theta, hover_data={}, labels={}, title='') -> FigureWidget:
     """
-    Helping function for assign_bins implementing binary search
-    :return: [bin_no, left_bound, right_bound]
+    Draws rose plot figure and returns it
     """
-    left, right = 0, len(limits) - 1
-    lefts, rights, mids = [left], [right], []
-    while left <= right:
-        mid = left + (right - left) // 2
-        mids.append(mid)
-
-        if right - left == 0:
-            # Return bin and interval
-            if val < limits[mid]:
-                return [mid, str(limits[mid - 1]), str(limits[mid])]
-            else:
-                right_interval = "inf" if (
-                    mid + 1) == len(limits) else str(limits[mid + 1])
-                return [mid + 1, str(limits[mid]), right_interval]
-
-        if val == limits[mid]:
-            # Return left bin
-            return [mid, str(limits[mid - 1]), str(limits[mid])]
-
-        if val < limits[mid]:
-            # Get left half
-            right = mid - 1
-            lefts.append(-1)
-            rights.append(right)
-        else:
-            # Get right half
-            left = mid + 1
-            lefts.append(left)
-            rights.append(-1)
-
-    return [mid, str(limits[mid - 1]), str(limits[mid])]
-
-
-def assign_bins(values: pd.Series, no_bins, column_names) -> pd.DataFrame:
-    """
-    Assigns values to bins [1; no_bins]
-    :return: DataFrame with three columns: bin, left_bound, right_bound
-    """
-    limits = np.linspace(values.min(), values.max(), no_bins)
-    return pd.DataFrame(
-        np.array([_find_bin(limits, val) for val in values.values]),
-        columns=column_names
-    )
-
-
-def plot_rose(df: pd.DataFrame, r, theta, hover_data={}, labels={}, title='') -> None:
-    # TODO: Add docstring
     fig = px.bar_polar(
         df, r=r, theta=theta, labels=labels,
         title=title, start_angle=0, template='ggplot2',
@@ -73,31 +33,49 @@ def plot_rose(df: pd.DataFrame, r, theta, hover_data={}, labels={}, title='') ->
         direction='counterclockwise'
     )
 
-    fig.show()
+    return fig
 
 
-if __name__ == '__main__':
+def plot_mapbox(df: pd.DataFrame, lat, lon, text, center: list, size=None, color=None, labels={}) -> FigureWidget:
+    """
+    Draws mapbox plot figure and returns it
+    """
+    fig = px.scatter_mapbox(
+        df, lat=lat, lon=lon, size=size, color=color, text=text, zoom=10,
+        template='plotly', mapbox_style='basic', labels=labels
+    )
+
+    fig.update_traces(
+        hovertemplate="(%{lat}, %{lon})<br>%{text}<extra></extra>"
+    )
+
+    c_lat, c_lon = center
+
+    fig.update_layout(
+        mapbox=dict(
+            center=go.layout.mapbox.Center(
+                lat=c_lat,
+                lon=c_lon
+            )
+        ),
+        margin={'r': 5, 'l': 5, 't': 30, 'b': 5}
+    )
+
+    return fig
+
+
+def get_rose_plot():
     if file_exists(bin_file):
         bins = pd.read_csv(bin_file)
         bins['left_bound'] = bins['left_bound'].astype(str)
     else:
-        df = pd.read_csv(data_dir + 'reduced.csv')
-
-        columns_names = ["bin", "left_bound", "right_bound"]
-        bins = assign_bins(df['distance'], no_bins, columns_names)
-
-        bins['bin'] = pd.to_numeric(bins['bin'])
-        bins['interval'] = " " + bins["left_bound"] + \
-            " - " + bins["right_bound"]
-
-        bins.to_csv(bin_file, index=False)
+        create_bins(pd.read_csv(data_dir + 'reduced.csv'), no_bins)
 
     distances = bins.groupby(by=['bin', 'left_bound', 'interval']) \
         .count()                                                   \
         .reset_index()                                             \
         .sort_values(by=['bin'])                                   \
         .rename(columns={'right_bound': 'count'})
-    print(distances.info())
 
     hover_data = {
         'interval': True,
@@ -110,4 +88,34 @@ if __name__ == '__main__':
     }
     title = 'Distance plot, something...'
 
-    plot_rose(distances, 'count', 'left_bound', hover_data, labels, title)
+    return plot_rose(distances, 'count', 'left_bound', hover_data, labels, title)
+
+
+def get_mapbox_plot():
+    stations = pd.read_csv(data_dir + 'stations.csv')
+
+    stations.replace({'traffic': {
+        'very high': 'Very high',
+        'high': 'High',
+        'moderate': 'Moderate',
+        'low': 'Low',
+        'very low': 'Very low'
+    }}, inplace=True)
+
+    px.set_mapbox_access_token(open(mapbox_token).read())
+
+    labels = {'traffic': "Traffic:"}
+
+    return plot_mapbox(
+        df=stations, lat='departure_latitude', lon='departure_longitude',
+        text='departure_name', center=[60.19525, 24.9013], size='count',
+        color='traffic', labels=labels
+    )
+
+
+if __name__ == '__main__':
+    rose = get_rose_plot()
+    rose.show()
+
+    mapbox = get_mapbox_plot()
+    mapbox.show()
